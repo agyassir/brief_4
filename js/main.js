b4960802530dff9a1477dc745780a32632c2961f
elkhailihamza <elkhailihamza331@gmail.com>
2023-11-01 15:43:09 +0100

    made categories section and 30% off section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-$('.carousel').carousel({
-    interval: 2000
+let showMore = document.getElementById('show-more');
+let arrow = document.getElementsByClassName('arrow');
+
+showMore.addEventListener('click', () => {
+    if (showMore.innerText == 'show more') {
+        showMore.innerText = 'show less';
+        arrow[0].style.display = 'block';
+        arrow[1].style.display = 'none';
+    } else {
+        showMore.innerText = 'show more';
+        arrow[0].style.display = 'none';
+        arrow[1].style.display = 'block';
+    }
 })
